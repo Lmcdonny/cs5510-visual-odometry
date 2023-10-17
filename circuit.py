3d64bb6786dfa2d99549f470872de592f719c6c3
Maximas5 <rabidyahoo1@gmail.com>
2023-10-17 17:13:21 -0600

    circuit.py maybe in a workable state

--- circuit.py
+++ circuit.py
@@ -1,36 +1,27 @@
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput
 from Car import Car
 import time
-from multiprocessing import Process
-import cv2
 
 # setup
 car = Car()
 
 # Go in circle and record
+# record
 picam2 = Picamera2()
-picam2.start()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+encoder = H264Encoder(bitrate=10000000)
+output = FileOutput('test.h264')
+picam2.start_recording(encoder, output)
 
+# circle
 car.control_car(50, 10)
+
 time.sleep(3)
+
 car.stop()
-
-circuit_pics = picam2.capture_array()
-
-##### Write video #####
-frame_size = (circuit_pics[0].shape[1], circuit_pics[0].shape[0])
-
-# Define the codec and create a VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can use other codecs like 'XVID' or 'MJPG' based on your needs
-out = cv2.VideoWriter('circuit.mp4', fourcc, 30, frame_size)  # 30 frames per second, adjust as needed
-
-# Loop through the image files and add them to the video
-for image_file in circuit_pics:
-    img = cv2.imread(image_file)
-    out.write(img)
-
-# Release the VideoWriter object and close the display window if any
-out.release()
-cv2.destroyAllWindows()
+picam2.stop_recording()
 
 del car
