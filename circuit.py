bd74d4e22d670b4ffc25e42327bbca6f07fdd592
boombot <pi@yahboom.boom>
2023-10-18 08:24:07 +0800

    Changed from picamera2 to picamera

--- circuit.py
+++ circuit.py
@@ -1,7 +1,7 @@
-from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput
-from Car import Car
+from picamera import Picamera
+from picamera.encoders import H264Encoder
+from picamera.outputs import FileOutput
+from ~/raspbot_code/Car import Car
 import time
 
 # setup
@@ -9,12 +9,12 @@ car = Car()
 
 # Go in circle and record
 # record
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration()
-picam2.configure(video_config)
+picam = Picamera()
+video_config = picam.create_video_configuration()
+picam.configure(video_config)
 encoder = H264Encoder(bitrate=10000000)
 output = FileOutput('test.h264')
-picam2.start_recording(encoder, output)
+picam.start_recording(encoder, output)
 
 # circle
 car.control_car(50, 10)
