0663e21b4cb47fcc753710d0fed5a3b069a4dac0
Maximas5 <rabidyahoo1@gmail.com>
2023-10-18 17:29:03 -0600

    Cleaned up files that are no longer needed

--- utils/Commands.py
+++
@@ -1,9 +0,0 @@
-'''
-Just an easy way to syncronize commands between server and client
-'''
-FORWARD = "0"
-BACKWARD = "1"
-LEFT = "2"
-RIGHT = "3"
-STOP = "4"
-QUIT = "5"
--- utils/Net_Info.py
+++
@@ -1,7 +0,0 @@
-'''
-An easy way to keep track of network information
-'''
-ROBOT_IP = '10.0.0.4'
-CLIENT_IP = '10.0.0.3'
-CONTROL_PORT = 12345
-IMAGE_PORT = 12346
